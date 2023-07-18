be5f53f69689c5544d35bc1a85600cd4e42a2bc0
sajeethajinu <sajeetha88@gmail.com>
2023-07-18 15:36:58 +0530

    first commit

---
+++ taskapp/urls.py
@@ -0,0 +1,14 @@
+from . import views
+from django.urls import path,include
+
+urlpatterns = [
+
+    path('', views.add, name='add'),
+    # path('details',views.details,name='details'),
+    path('delete/<int:taskid>/', views.delete, name='delete'),
+    path('update/<int:id>/', views.update, name='update'),
+    path('cbvindex/',views.Tasklistview.as_view(),name='cbvindex'),
+    path('cbvdetails/<int:pk>/',views.Taskdetailview.as_view(),name='cbvdetails'),
+    path('cbvupdate/<int:pk>/',views.Taskupdateview.as_view(),name='cbvupdate'),
+    path('cbvdelete/<int:pk>/',views.Taskdeleteview.as_view(),name='cbvdelete'),
+]
